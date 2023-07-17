e2c7e565726e367ea609f308da62467928fa0718
Finn Ickler <finnickler@gmail.com>
2023-07-17 16:26:25 +0200

    Added information to API

--- WcaOnRails/app/models/competition.rb
+++ WcaOnRails/app/models/competition.rb
@@ -1880,7 +1880,7 @@ class Competition < ApplicationRecord
            "on_the_spot_registration","on_the_spot_entry_fee_lowest_denomination","base_entry_fee_lowest_denomination","currency_code","allow_registration_edits",
            "allow_registration_self_delete_after_acceptance","allow_registration_without_qualification","refund_policy_percent",
            "use_wca_registration","guests_per_registration_limit",
-           "guests_entry_fee_lowest_denomination","guest_entry_status"],
+           "guests_entry_fee_lowest_denomination","guest_entry_status","information"],
     methods: ["url", "website", "short_name", "city", "venue_address",
               "venue_details", "latitude_degrees", "longitude_degrees",
               "country_iso2", "event_ids","registration_opened?","main_event_id"],
