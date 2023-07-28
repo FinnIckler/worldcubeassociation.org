75e812f16a0bb36cc621aae803113dfe49540782
FinnIckler <finnickler@gmail.com>
2023-07-28 17:36:28 +0200

    remove protect from forgery for testing

--- WcaOnRails/app/controllers/application_controller.rb
+++ WcaOnRails/app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ require "newrelic_rpm"
 
 class ApplicationController < ActionController::Base
   include TimeWillTell::Helpers::DateRangeHelper
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
 
   before_action :add_new_relic_headers, :set_locale
   protected def add_new_relic_headers
