071cb7fca9276f7da8842d2dc692ff31406d75f2
FinnIckler <finnickler@gmail.com>
2023-08-01 13:22:49 +0200

    Include rounds better

--- WcaOnRails/app/models/competition.rb
+++ WcaOnRails/app/models/competition.rb
@@ -1650,6 +1650,15 @@ class Competition < ApplicationRecord
       .map(&:to_wcif)
   end
 
+  def events_with_rounds
+    includes_associations = [
+      { rounds: [:competition_event] },
+    ]
+    competition_events
+      .includes(includes_associations)
+      .sort_by { |ce| ce.event.rank }
+  end
+
   def schedule_wcif
     competition_venues_includes_associations = [
       {
@@ -1879,8 +1888,8 @@ class Competition < ApplicationRecord
 
   DEFAULT_SERIALIZE_OPTIONS = {
     only: %w[id name website start_date registration_open registration_close announced_at cancelled_at end_date competition_venues competitor_limit extra_registration_requirements enable_donations refund_policy_limit_date event_change_deadline_date waiting_list_deadline_date on_the_spot_registration on_the_spot_entry_fee_lowest_denomination qualification_results event_restrictions base_entry_fee_lowest_denomination currency_code allow_registration_edits allow_registration_self_delete_after_acceptance allow_registration_without_qualification refund_policy_percent use_wca_registration guests_per_registration_limit venue contact force_comment_in_registration use_wca_registration external_registration_page guests_entry_fee_lowest_denomination guest_entry_status information],
-    methods: %w[url website short_name city venue_address venue_details latitude_degrees longitude_degrees competition_events country_iso2 event_ids registration_opened? main_event_id number_of_bookmarks using_stripe_payments? uses_qualification?],
-    include: ['delegates', 'organizers', 'tabs', { rounds: [:competition_event] }],
+    methods: %w[url website short_name city venue_address venue_details latitude_degrees longitude_degrees competition_events country_iso2 event_ids registration_opened? main_event_id number_of_bookmarks using_stripe_payments? uses_qualification? events_with_rounds],
+    include: ['delegates', 'organizers', 'tabs'],
   }.freeze
 
   def serializable_hash(options = nil)
