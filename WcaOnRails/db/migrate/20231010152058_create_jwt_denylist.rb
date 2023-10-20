5a72cd35a8f016de36d7848c43bc3db66dbc0bf8
Finn Ickler <finnickler@gmail.com>
2023-10-20 18:04:43 +0200

    correctly name create_jwt_denylist

--- WcaOnRails/db/migrate/20231010152058_create_jwt_denylist.rb
+++ WcaOnRails/db/migrate/20231010152058_create_jwt_denylist.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddJwtDenylist < ActiveRecord::Migration[7.0]
+class CreateJwtDenylist < ActiveRecord::Migration[7.0]
   def change
     create_table :jwt_denylist do |t|
       t.string :jti, null: false
