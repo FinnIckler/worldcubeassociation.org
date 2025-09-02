c84e3ed85917f4eb9e33ab02a3b74c1a57d8bcb6
FinnIckler <finnickler@gmail.com>
2025-09-02 16:44:02 +0200

    add scrambles route

---
+++ next-frontend/src/lib/wca/competitions/getScrambles.ts
@@ -0,0 +1,8 @@
+import { serverClient } from "@/lib/wca/wcaAPI";
+import { cache } from "react";
+
+export const getScrambles = cache(async (competitionId: string) => {
+  return await serverClient.GET("/competitions/{competitionId}/scrambles", {
+    params: { path: { competitionId } },
+  });
+});
